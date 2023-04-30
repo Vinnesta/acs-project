8a11322bfff5012c2827a519b88fa90de6e4d3a8
Bruce <brucecheung@users.noreply.github.com>
2023-04-30 16:16:15 +0100

    Add dataset functions to utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import Image, ImageDraw
 import re
+import torch
+import torch.nn.functional as F
 
 class ColoursUtil():
   def hsl_to_hsv(hsl):
@@ -124,3 +126,122 @@ class Tokeniser():
           if word.endswith(ending):
               return [word[:-len(ending)], '+' + ending]
       return [word]
+      
+      
+class ColourDataset(Dataset):
+  def __init__(self, x, c, y, colour_vector_dim):
+    self.x = x
+    self.c = c
+    self.y = y
+    self.colour_vector_dim = colour_vector_dim
+    self.c_vec = self.vectorise_colours()
+
+  def vectorise_colours(self) -> torch.Tensor:
+    resolution = [3]
+    c_np = self.c.cpu().detach().numpy()
+    n = c_np.shape[0]
+    assert(c_np.shape[1] == 3)
+    assert(c_np.shape[2] == 3)
+    return torch.Tensor(np.reshape(ColoursUtil.vectorize_all(np.reshape(c_np, (n*3, 3)), resolution), (n, 3, self.colour_vector_dim)))
+  
+  def __len__(self):
+    return self.x.shape[0]
+
+  def __getitem__(self, idx):
+    return (self.x[idx], self.c_vec[idx], self.c[idx]), self.y[idx]
+
+class DatasetFactory():
+  def __init__(self, max_seq_len, colour_vector_dim, start_token, end_token):
+    self.max_seq_len = max_seq_len
+    self.colour_vector_dim = colour_vector_dim
+    self.start_token = start_token
+    self.end_token = end_token
+
+  def process_listener_data(self, df, vocab):
+    # Convert speaker utterances into sequences of one-hot encoded embeddings
+    # But rather than actual one-hot vectors, just use the vocabulary index for each token
+    # Dim: Number of samples * MAX_SEQ_LEN
+    X = []
+    for text in df['allSpeakerContents']:
+      text_tensor = torch.tensor(vocab(Tokeniser.heuristic_ending_tokenizer(text)))
+      text_tensor = text_tensor[:self.max_seq_len]
+      
+      # Pad the text tensor to MAX_SEQ_LEN so that every sample has the same sequence length
+      pad_length = self.max_seq_len - len(text_tensor)
+      text_tensor = F.pad(text_tensor, (0, pad_length))
+      X.append(text_tensor)
+
+    X = torch.stack(X)
+
+    # Get HSL colour context of each sample
+    # Dim: Number of samples * number of colours in context (3) * HSL values (3)
+    targetH = df['targetH'].tolist()
+    targetS = df['targetS'].tolist()
+    targetL = df['targetL'].tolist()
+    distr1H = df['distr1H'].tolist()
+    distr1S = df['distr1S'].tolist()
+    distr1L = df['distr1L'].tolist()
+    distr2H = df['distr2H'].tolist()
+    distr2S = df['distr2S'].tolist()
+    distr2L = df['distr2L'].tolist()
+
+    targets = torch.transpose(torch.tensor([targetH, targetS, targetL]), dim0=0, dim1=1)
+    distractors1 = torch.transpose(torch.tensor([distr1H, distr1S, distr1L]), dim0=0, dim1=1)
+    distractors2 = torch.transpose(torch.tensor([distr2H, distr2S, distr2L]), dim0=0, dim1=1)
+    C = torch.stack([targets, distractors1, distractors2], dim=1)
+
+    # Set the target labels as zero for all samples, since the first colour in context is the target
+    # Dim: Number of samples
+    Y = torch.zeros(X.shape[0], dtype=torch.long)
+
+    dataset = ColourDataset(x=X, c=C, y=Y, colour_vector_dim=self.colour_vector_dim)
+    return dataset
+    
+  def process_speaker_data(self, df, vocab, orig_colour_order=False):
+    # Convert speaker utterances into sequences of one-hot encoded embeddings
+    # But rather than actual one-hot vectors, just use the vocabulary index for each token
+    # Dim: Number of samples * MAX_SEQ_LEN
+    X = []
+    Y = []
+    for text in df['allSpeakerContents']:
+      text_tokens = Tokeniser.heuristic_ending_tokenizer(text)
+      text_tokens.insert(0, self.start_token)
+      text_tensor = torch.tensor(vocab(text_tokens))
+      text_tensor = text_tensor[:self.max_seq_len+1] # Add 1 here to account for the START_TOKEN
+
+      # Pad the text tensor to MAX_SEQ_LEN so that every sample has the same sequence length
+      pad_length = self.max_seq_len + 1 - len(text_tensor)
+      x_tensor = F.pad(text_tensor, (0, pad_length))
+      X.append(x_tensor)
+
+      # To get target label y, remove START_TOKEN and add END_TOKEN
+      text_tensor = torch.cat((text_tensor[1:], torch.tensor(vocab([self.end_token]))))
+      y_tensor = F.pad(text_tensor, (0, pad_length))
+      Y.append(y_tensor)
+
+    X = torch.stack(X)
+    Y = torch.stack(Y)
+
+    # Get HSL colour context of each sample
+    # Dim: Number of samples * number of colours in context (3) * HSL values (3)
+    targetH = df['targetH'].tolist()
+    targetS = df['targetS'].tolist()
+    targetL = df['targetL'].tolist()
+    distr1H = df['distr1H'].tolist()
+    distr1S = df['distr1S'].tolist()
+    distr1L = df['distr1L'].tolist()
+    distr2H = df['distr2H'].tolist()
+    distr2S = df['distr2S'].tolist()
+    distr2L = df['distr2L'].tolist()
+
+    targets = torch.transpose(torch.tensor([targetH, targetS, targetL]), dim0=0, dim1=1)
+    distractors1 = torch.transpose(torch.tensor([distr1H, distr1S, distr1L]), dim0=0, dim1=1)
+    distractors2 = torch.transpose(torch.tensor([distr2H, distr2S, distr2L]), dim0=0, dim1=1)
+    
+    if orig_colour_order:
+      C = torch.stack([distractors1, distractors2, targets], dim=1) # Place `targets` at the end as per 'Colors in Context'
+    else:
+      C = torch.stack([targets, distractors1, distractors2], dim=1)
+
+    dataset = ColourDataset(x=X, c=C, y=Y, colour_vector_dim=self.colour_vector_dim)
+    return dataset
